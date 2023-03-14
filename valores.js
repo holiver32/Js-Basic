758be75f71b0a45228d04d20c9e66ab6a06f8a6a
Holiver Hurtado <Holiver.hurtado7@gmail.com>
2023-03-14 16:19:02 -0400

    first commit

---
+++ valores.js
@@ -0,0 +1,20 @@
+// Tipos de Datos Primitivos 
+40
+
+"Holiver Hurtado" // Strings (cadenas de texto), Esto es la forma en la que puedo crear algo que se pueda imprimir en la pantalla, o escribir información, que el usuario pueda entender y leer. utilizar palabras, o frases.
+
+
+true // Booleanos, estos fueron inventados por George Boole, un Matemático británico. y esto hace referencia a cosas que podamos validar.
+false
+
+null //Valores vacios (empty values), es como un placeholder (espacio reservado para algun valor) de algún valor faltante.
+
+undefined
+
+ /* ================================================================ */ 
+
+ //Tipos de Datos objetos o no primitivos 
+
+[1, 2, 3, 4, 5] //Corchetes //arrays
+{ nombre: 'Holiver' } //llaves //objetos
+
