5c81a7e23b2b6aa82556745cc1dc913940bd8c6f
Holiver Hurtado <Holiver.hurtado7@gmail.com>
2023-03-14 20:26:42 -0400

    clases y ejercicios del día + evaluación.

---
+++ Review.js
@@ -0,0 +1,12 @@
+//En este desafío encontrarás una función llamada solution que recibe un parámetro llamado valor. Debes encontrar el tipo de dato del parámetro valor y retornarlo desde la función solution.
+
+//Recuerda que el parámetro valor será distinto por cada distinta forma en que ejecutemos la función solution.
+
+
+export function solution(valor) {
+    return typeof valor;
+  
+    solution(1);
+    solution("Holiver");
+    solution(true);// Escribe tu código aquí 👈
+  }
