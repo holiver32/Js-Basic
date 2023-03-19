12724f1ae0b8fdfeacb696334cddcb7c90c85350
Holiver Hurtado <Holiver.hurtado7@gmail.com>
2023-03-18 22:21:00 -0400

    clases del día y 1 evaluación.

---
+++ Review-5.js
@@ -0,0 +1,15 @@
+//En eeste desafío vas a recibir un parámetro arraySecreto en la función solution. Debes retornar true si el primer elemento del arraySecreto es de tipo string y debes retornar false si es de cualquier otro tipo.
+
+//Input
+
+//solution(["Huevo", "Gallina", "Vaca"])
+//solution([1, "Gallina", "Vaca"])
+
+//Output
+
+//true
+//false
+export function solution(arraySecreto) {
+    return(typeof arraySecreto [0] === "string"? true:false)// Tu código aquí 👈
+  }
+  
